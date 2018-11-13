c8e37edc2e92c41a2e84549e1761509a06b5399c
sdreyes <sheshechan90@gmail.com>
2018-11-13 16:45:29 -0500

    Add function that replaces blanks with correct letter guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Array of possibilities for the word the user has to guess
-var wordChoices = ["pikachu", "charmander", "squirtle", "bulbasaur", "meowth", "jigglypuff", "chansey", "ghastly", "gengar", "mew"];
+const wordChoices = ["pikachu", "charmander", "squirtle", "bulbasaur", "meowth", "jigglypuff", "chansey", "ghastly", "gengar", "mew"];
 
 // Holds the score
 var wins = 0;
@@ -12,12 +12,25 @@ var guessesLeftText = document.getElementById("guessesleft-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 
+var wordChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+
+console.log(wordChoice);
+
+var blanks = [];
+
+//creates an array of under scores to show how many letters are in the word the user needs to guess
+for (var i=0; i < wordChoice.length; i++) {
+    blanks.push("_");
+}
+console.log(blanks);
+
 // When the user presses a key this function runs
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
-    var wordChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-
-    console.log(wordChoice);
+    if(wordChoice.indexOf(userGuess) > -1) {
+        blanks[wordChoice.indexOf(userGuess)] = userGuess;
+        console.log(blanks);
+    }
 
 }
