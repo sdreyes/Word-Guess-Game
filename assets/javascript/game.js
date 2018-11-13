a860411d5d017425b31b8b40af6ddfc77bc5e4bb
sdreyes <sheshechan90@gmail.com>
2018-11-13 17:12:25 -0500

    Place script reference at bottom of document and display number of guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ const wordChoices = ["pikachu", "charmander", "squirtle", "bulbasaur", "meowth",
 // Holds the score
 var wins = 0;
 var losses = 0;
+var guesses = 12;
 
 // Variables that hold text to display
 var wordText = document.getElementById("word-text");
@@ -16,21 +17,26 @@ var wordChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
 console.log(wordChoice);
 
-var blanks = [];
+var wordLetters = [];
+var wrongGuesses =[];
 
 //creates an array of under scores to show how many letters are in the word the user needs to guess
 for (var i=0; i < wordChoice.length; i++) {
-    blanks.push("_");
+    wordLetters.push("_");
 }
-console.log(blanks);
+console.log(wordLetters);
 
 // When the user presses a key this function runs
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
     if(wordChoice.indexOf(userGuess) > -1) {
-        blanks[wordChoice.indexOf(userGuess)] = userGuess;
-        console.log(blanks);
+        wordLetters[wordChoice.indexOf(userGuess)] = userGuess;
+        console.log(wordLetters);
+    } else {
+        wrongGuesses.push(userGuess);
+        guesses = guesses - 1;
+        console.log(wrongGuesses);
+        guessesLeftText.textContent = guesses;
     }
-
 }
