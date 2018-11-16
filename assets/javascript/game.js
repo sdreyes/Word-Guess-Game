13fcdc7c02f6b0d76a6845cd9d9af7a75f8d5a1b
sdreyes <sheshechan90@gmail.com>
2018-11-15 19:21:00 -0500

    Work on not counting duplicate letters toward duplicate guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -58,8 +58,15 @@ document.onkeyup = function (event) {
                 for (var j = 0; j < wordChoice.length; j++) {
                     // Does not deduct from "guesses left" if the letter was previously guessed
                     if (wordChoice[j] === userGuess && wordLetters[j] === "_") {
+                        // wordLetters.indexOf[j] > -1
                         wordLetters[j] = userGuess
                         guesses = guesses + 1;
+                        console.log(wordLetters);
+
+                        if (wordLetters.indexOf["_"] === -1 && guesses <= 12) {
+                            wins++;
+                            resetGame();
+                        }
                     }
                 }
 
